7ece4ab50d2299d5f04977b15121020457b1bea5
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2021-08-07 13:51:16 -0400

    Update frontend with env vars

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,7 +7,7 @@ const webpack = require("webpack");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = function(_env, argv) {
+module.exports = function (_env, argv) {
   const isProduction = argv.mode === "production";
   const isDevelopment = !isProduction;
 
@@ -17,7 +17,7 @@ module.exports = function(_env, argv) {
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: "assets/js/[name].[contenthash:8].js",
-      publicPath: "/"
+      publicPath: "/",
     },
     module: {
       rules: [
@@ -29,67 +29,70 @@ module.exports = function(_env, argv) {
             options: {
               cacheDirectory: true,
               cacheCompression: false,
-              envName: isProduction ? "production" : "development"
-            }
-          }
-        },
-        {
-        test: /\.(css|scss|sass)$/,
-        use: [
-           MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'postcss-loader', // postcss loader needed for tailwindcss
-            options: {
-              postcssOptions: {
-                ident: 'postcss',
-                plugins: [tailwindcss, autoprefixer],
-              },
+              envName: isProduction ? "production" : "development",
             },
           },
-          'sass-loader'
-        ],
-      },
+        },
+        {
+          test: /\.(css|scss|sass)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "postcss-loader", // postcss loader needed for tailwindcss
+              options: {
+                postcssOptions: {
+                  ident: "postcss",
+                  plugins: [tailwindcss, autoprefixer],
+                },
+              },
+            },
+            "sass-loader",
+          ],
+        },
         {
           test: /\.(png|jpg|gif)$/i,
           use: {
             loader: "url-loader",
             options: {
               limit: 8192,
-              name: "static/media/[name].[hash:8].[ext]"
-            }
-          }
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
         },
         {
           test: /\.svg$/,
-          use: ["@svgr/webpack", "url-loader"]
+          use: ["@svgr/webpack", "url-loader"],
         },
         {
           test: /\.(eot|otf|ttf|woff|woff2)$/,
           loader: require.resolve("file-loader"),
           options: {
-            name: "static/media/[name].[hash:8].[ext]"
-          }
-        }
-      ]
+            name: "static/media/[name].[hash:8].[ext]",
+          },
+        },
+      ],
     },
     resolve: {
-      extensions: [".js", ".jsx"]
+      extensions: [".js", ".jsx"],
     },
     plugins: [
       new MiniCssExtractPlugin({
-          filename: "assets/css/[name].[contenthash:8].css",
-          chunkFilename: "assets/css/[name].[contenthash:8].chunk.css"
+        filename: "assets/css/[name].[contenthash:8].css",
+        chunkFilename: "assets/css/[name].[contenthash:8].chunk.css",
       }),
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "public/index.html"),
-        inject: true
+        inject: true,
       }),
       new webpack.DefinePlugin({
         "process.env.NODE_ENV": JSON.stringify(
           isProduction ? "production" : "development"
-        )
-      })
+        ),
+      }),
+      new webpack.DefinePlugin({
+        "process.env.API_URL": `"${process.env.API_URL}"`,
+      }),
     ].filter(Boolean),
     optimization: {
       minimize: isProduction,
@@ -97,19 +100,19 @@ module.exports = function(_env, argv) {
         new TerserWebpackPlugin({
           terserOptions: {
             compress: {
-              comparisons: false
+              comparisons: false,
             },
             mangle: {
-              safari10: true
+              safari10: true,
             },
             output: {
               comments: false,
-              ascii_only: true
+              ascii_only: true,
             },
-            warnings: false
-          }
+            warnings: false,
+          },
         }),
-        new OptimizeCssAssetsPlugin()
+        new OptimizeCssAssetsPlugin(),
       ],
       splitChunks: {
         chunks: "all",
@@ -124,21 +127,21 @@ module.exports = function(_env, argv) {
                 /[\\/]node_modules[\\/](.*?)([\\/]|$)/
               )[1];
               return `${cacheGroupKey}.${packageName.replace("@", "")}`;
-            }
+            },
           },
           common: {
             minChunks: 2,
-            priority: -10
-          }
-        }
+            priority: -10,
+          },
+        },
       },
-      runtimeChunk: "single"
+      runtimeChunk: "single",
     },
     devServer: {
       compress: true,
       historyApiFallback: true,
       open: true,
-      overlay: true
-    }
+      overlay: true,
+    },
   };
 };
